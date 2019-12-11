f72479e3fa2cf40f2547eb7a8b5e6997ad299f24
cheny_company <chenyou920@gmail.com>
2019-12-11 18:09:26 +0800

    对象注入

---
+++ src/List_and_Array/ArrayTest.java
@@ -0,0 +1,8 @@
+package List_and_Array;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        String[] strArr1 = new String[10];
+        System.out.println(strArr1.length);
+    }
+}
