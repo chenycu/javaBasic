8aef0e45d27240c168ce10c143e168a40b5ef324
chenyou <you92@vip.qq.com>
2021-06-02 00:02:32 +0800

    0

--- src/_git/GitTest_2.java
+++
@@ -1,7 +0,0 @@
-package _git;
-
-public class GitTest_2 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/_git/GitTest_4.java
+++
@@ -1,7 +0,0 @@
-package _git;
-
-public class GitTest_4 {
-    public static void main(String[] args) {
-        System.out.println(4);
-    }
-}
