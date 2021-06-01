b4e7b2da73bed04a76f27621369449430579c446
chenyou <you92@vip.qq.com>
2021-06-01 23:08:49 +0800

    Revert "1"
    This reverts commit fb012a2a

--- src/_git/GitTest.java
+++ src/_git/GitTest.java
@@ -1,10 +1,8 @@
 package _git;
 
-public class GitTest_1 {
+public class GitTest {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
-        
-
     }
 }
